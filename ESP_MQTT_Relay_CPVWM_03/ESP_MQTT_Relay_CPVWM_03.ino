8494ca1f13d7a72d81f7fa9aafcab6edb196a1c1
Samuele Vecchi <samuele.vecchi@gmail.com>
2017-06-19 13:19:19 +0200

    added relay shield

---
+++ ESP_MQTT_Relay_CPVWM_03/ESP_MQTT_Relay_CPVWM_03.ino
@@ -0,0 +1,136 @@
+/*
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+#define PRESMSGINTVL 30000
+
+const int relayPin = D1;
+int relayStatus = 0;
+
+const char* ssid     = "comPVter";
+const char* password = ""; //add wifi password here!
+
+const char* mqtt_server = ""; //Add mqtt broker here
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastPresMsg = 0;
+char msg[60];
+int value = 0;
+
+const char* nodeId = "CPVWM##"; // ## unique serial number of the node! 01,02...
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  digitalWrite(BUILTIN_LED,HIGH);
+  Serial.begin(115200);
+  setup_wifi();
+  
+  pinMode(relayPin, OUTPUT);
+  
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String  message = "";
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.print(message);
+  Serial.println();
+
+  StaticJsonBuffer<100> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.parseObject(message);
+  
+  String trigger = root["trigger"];   Serial.print("trigger: ");    Serial.println(trigger);
+  if (trigger == "double")
+  {
+    Serial.println("double click");
+    if (relayStatus == 0)
+    {
+      relayStatus = 1;
+      digitalWrite(relayPin, HIGH);
+    }
+    else
+    {
+      relayStatus = 0;
+      digitalWrite(relayPin, LOW);
+    }
+  }
+    if (trigger == "long")
+  {
+    Serial.println("Long click");
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    //boolean connect(const char* id, const char* willTopic, uint8_t willQos, boolean willRetain, const char* willMessage);
+    sprintf(msg, nodeId);
+    sprintf(&msg[7], " is dead");
+    if (client.connect(nodeId, "compvter/LWT", MQTTQOS2, false, msg)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("compvter/presentations", nodeId);
+      // ... and resubscribe
+      client.subscribe("compvter/CPVWM03/actions");
+      client.subscribe("compvter/actions");
+      digitalWrite(BUILTIN_LED,LOW);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop()
+{
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long int now = millis();
+  if ( (now - lastPresMsg) > PRESMSGINTVL)
+  {
+    lastPresMsg = now;
+    client.publish("compvter/presentations", nodeId);
+  }
+}
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 repository contenente codice presentato nel corso "we mos" :)
 13 giugno 2017: Wemos, arduino IDE e MQTT
 
-ESP_MQTT_DHT11_CPVWM_1.ino -> Sketch per wemos D1mini. Legge dato sentore DHT11 e lo pubblica via MQTT. 
+ESP_MQTT_DHT11_CPVWM_1.ino   -> Sketch per wemos D1mini. Legge dato sentore DHT11 e lo pubblica via MQTT. 
+ESP_MQTT_BUTTON_CPVWM_02.ino -> Sketch per wemos D1mini. Manda azioni pulsante via MQTT
